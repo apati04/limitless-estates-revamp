4eb64f5f4982ab721d436dea24d2c97c0241ffa1
Andrew Patipaksiri <apax714@gmail.com>
2019-03-12 11:36:53 -0700

    build

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const helmet = require("helmet");
 const compression = require("compression");
-
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(helmet());
