8dd77bb9abf0e196132f225af3e082ff107b15d1
Andrew Patipaksiri <apax714@gmail.com>
2019-03-16 16:43:37 -0700

    wizard skeleton

--- client/src/components/Layouts/Forms/WizardForm/Wiz.js
+++ client/src/components/Layouts/Forms/WizardForm/Wiz.js
@@ -2,8 +2,34 @@ import React, { Component } from 'react';
 
 class Wiz extends Component {
   state = { pageIndex: 0 };
+  _back = () => {
+    this.setState(prevState => ({
+      pageIndex: prevState.pageIndex - 1 < 0 ? prevState.pageIndex : 0
+    }));
+  };
+  _next = () => {
+    this.setState(prevState => ({ pageIndex: prevState.pageIndex + 1 }));
+  };
+  _renderPage = formProps => {
+    const { pageIndex } = this.state;
+    const Page = this.props.pages[pageIndex];
+    return (
+      <Page
+        {...formProps}
+        navigateBack={this._back}
+        navigateNext={this._next}
+      />
+    );
+  };
   render() {
-    return <div />;
+    const renderProps = {
+      pageIndex: this.state.pageIndex,
+      renderPage: this._renderPage,
+      navigateBack: this._back,
+      navigateNext: this._next
+    };
+
+    return this.props.children(renderProps);
   }
 }
 
