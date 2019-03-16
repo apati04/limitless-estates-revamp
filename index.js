93d8b3f5dbe5795ebfee5d08963677453a8ddfac
Andrew Patipaksiri <apax714@gmail.com>
2019-03-15 18:16:24 -0700

    build

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const helmet = require("helmet");
-const compression = require("compression");
-const path = require("path");
+const express = require('express');
+const helmet = require('helmet');
+const compression = require('compression');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(helmet());
@@ -9,12 +9,12 @@ app.use(compression());
 app.use(express.json());
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(__dirname, "client", "build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 app.listen(PORT, () => {
-  console.log("server on port ", PORT);
+  console.log('server on port ', PORT);
 });
