bb840b8829ab7e78f91df6eb04f4808d7267fa71
Andrew Patipaksiri <apax714@gmail.com>
2019-04-12 17:25:21 -0700

    build

--- server/config/prod.js
+++ server/config/prod.js
@@ -6,7 +6,7 @@ module.exports = {
   meetupOauthKey: process.env.MEETUP_OAUTH_KEY,
   meetupOauthSecret: process.env.MEETUP_OAUTH_SECRET,
   meetupOauthCallback: process.env.MEETUP_OAUTH_CALLBACK,
-  oauthBaseUrl: process.env.OAUTH_BASE_URL
+  oauthBaseUrl: process.env.OAUTH_BASE_URL,
   mapboxToken: process.env.MAPBOX_TOKEN,
   podcastAPIKey: process.env.PODCAST_API_KEY,
   podcastIdKey: process.env.PODCAST_ID_KEY
