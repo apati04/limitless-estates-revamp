69273eed6a54092aa874ce87cb000d88edcdd8af
Andrew Patipaksiri <apatipaksiri@where2getit.com>
2019-08-18 10:43:47 -0700

    remove old files

--- client/src/components/Header/Navigation-old/NavItems/primary-routes.js
+++
@@ -1,8 +0,0 @@
-import { AboutLink, QuestionnaireLink, PodcastLink, ContactLink } from '../routes';
-
-export default {
-	About: AboutLink,
-	Questionnaire: QuestionnaireLink,
-	Podcast: PodcastLink,
-	Contact: ContactLink
-};
