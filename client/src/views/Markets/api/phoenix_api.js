acf7dad2892e9e3ae6bcbc1b7d1a708463724476
Andrew Patipaksiri <apatipaksiri@where2getit.com>
2020-02-05 20:45:13 -0800

    add features

--- client/src/views/Markets/api/phoenix_api.js
+++ client/src/views/Markets/api/phoenix_api.js
@@ -1,41 +1,47 @@
 export default {
-  headerImageUrl: 'https://i.imgur.com/64Z6LZY.jpg',
-  title: 'Phoenix',
-  headline: 'Arizona',
-  snippet:
-    "Phoenix is located in the heart of the fastest growing and most dynamic metro area in the country. As of the Census Bureau's 2017 population estimates, metro Phoenix had 4,737,270 residents, making it the 12th largest metro area in the nation by population.",
-  maps: {
-    geo: { lat: 33.4476487, lng: -112.0743701 },
-    height: '320px',
-    width: '100%',
-    zoomLevel: 13
-  },
-  content: [
-    { title: { value: 'Phoenix, Arizona' } },
-    {
-      p: {
-        value:
-          "Phoenix is located in the heart of the fastest growing and most dynamic metro area in the country. As of the Census Bureau's 2017 population estimates, metro Phoenix had 4,737,270 residents, making it the 12th largest metro area in the nation by population. It is also one of the fastest growing major metro areas, gaining nearly 400,000 residents from 2010 to 2015, and more than 1.3 million since 2000. The population of the Phoenix metro area increased by 45.3% from 1990 through 2000, compared to the average United States rate of 13.2%."
-      }
+    headerImageUrl: 'https://i.imgur.com/64Z6LZY.jpg',
+    title: 'Phoenix',
+    headline: 'Arizona',
+    snippet:
+        "Phoenix is located in the heart of the fastest growing and most dynamic metro area in the country. As of the Census Bureau's 2017 population estimates, metro Phoenix had 4,737,270 residents, making it the 12th largest metro area in the nation by population.",
+    maps: {
+        geo: { lat: 33.4476487, lng: -112.0743701 },
+        height: '320px',
+        width: '100%',
+        zoomLevel: 13,
     },
-    {
-      p: {
-        value:
-          "The Phoenix metro area has historically been the center of the state's economy. As with the state of Arizona, the area relied on the 5 C's (copper, cattle, climate, citrus, and cotton) for its economic growth and expansion. A thriving job market is the driving force behind the city’s renaissance, with high-income positions being created in the metro area. From July 2016 to July 2017, 52,000 jobs were added in the Phoenix metro area, with 11,900 of them in the medical and education fields.[1] Money Magazine named Phoenix No. 3 among the country’s hottest cities for tech jobs in 2017, and Intel has announced that it intends to invest $7 billion in suburban Chandler, creating 3,000 new jobs where it already employs 11,300 people.[2] Other major employers planning to add jobs in Phoenix include JPMorgan Chase, which is adding 4,000 jobs, State Farm, UPS, ADP, and Santander Consumer USA."
-      }
-    },
-    {
-      p: {
-        value:
-          "Rental demand is being aided by strong population growth in the Phoenix metro area. Besides professional families, retirees continue to move to Phoenix in considerable numbers, and that demographic is expected to grow significantly. A study by the National Multifamily Housing Council and National Apartment Association found that the area will need 150,302 new apartments by 2030. That is a 38% increase over the current number. In early 2013, Forbes ranked the Phoenix metro area as the 8th fastest growing major city in the United States, after Austin, Houston, Dallas, Raleigh, Salt Lake City, Seattle and Provo. By 2020, it's forecasted that Phoenix will become the 4th most populous city in the US, and by 2030, the US Census Bureau estimates its population will reach 2.2 million with a metro area that's home to 6.3 million."
-      }
-    },
-    {
-      ul: [
-        'Population, job and rental rate growth all outpacing national average',
-        'Ranked 5th in U.S. for high-tech job growth and Top 10 fastest growing economies',
-        '3rd largest labor pool in the West'
-      ]
-    }
-  ]
+    content: [
+        { title: { value: 'Phoenix, Arizona' } },
+        {
+            p: {
+                value:
+                    "Phoenix is located in the heart of the fastest growing and most dynamic metro area in the country. As of the Census Bureau's 2017 population estimates, Metro Phoenix had 4,737,270 residents, making it the 11th largest Metropolitan Area in the nation by population.  It is also one of the fastest growing major metro areas, gaining nearly 600,000 residents from 2010 to 2017, and more than 1.4 million since 2000. The population of the Phoenix metro area increased by 45.3% from 1990 through 2000, compared to the average United States rate of 13.2%.",
+            },
+        },
+        {
+            p: {
+                value:
+                    "The Phoenix metro area has historically been the center of the state's economy. As with the state of Arizona, the area relied on the 5 C's (copper, cattle, climate, citrus, and cotton) for its economic growth and expansion. A thriving job market is the driving force behind the city’s renaissance, with high-income positions being created in the metro area. From July 2016 to July 2017, 52,000 jobs were added in the Phoenix metro area, with 11,900 of them in the medical and education fields.  Money Magazine named Phoenix No. 3 among the country’s hottest cities for tech jobs in 2017, and Intel has announced that it intends to invest $7 billion in suburban Chandler, creating 3,000 new jobs where it already employs 11,300 people. Several Fortune 500 and Fortune 1000 companies have their international headquarters in the area, including Avnet, PetSmart, Apollo Education Group, Republic Services, ON Semiconductor, and Sprouts Farmers Market. Other Fortune 500 companies with significant presence include Banner Health, the state's largest private employer, American Airlines, which merged with Tempe-based US Airways, American Express, Wells Fargo, Boeing, and Intel, which has a large regional campus in Chandler.",
+            },
+        },
+        {
+            p: {
+                value:
+                    "Rental demand is being aided by strong population growth in the Phoenix metro area. Besides professional families, retirees continue to move to Phoenix in considerable numbers, and that demographic is expected to grow significantly. A study by the National Multifamily Housing Council and National Apartment Association found that the area will need 150,302 new apartments by 2030. That is a 38% increase over the current number.  By 2020, it's forecasted that Phoenix will become the 4th most populous city in the US, and by 2030, the US Census Bureau estimates its population will reach 2.2 million with a metro area that's home to 6.3 million.",
+            },
+        },
+        {
+            ul: [
+                'Population, job and rental rate growth all outpacing national average',
+                'Ranked 5th in U.S. for high-tech job growth and Top 10 fastest growing economies',
+                '3rd largest labor pool in the West',
+                'The Phoenix Metro has welcomed 47+ corporate headquarter relocations in the last 3 years',
+                'Ranked 3rd in U.S. for high-tech job growth and 3rd largest labor pool in the West',
+                'Top 10 fastest growing economies and 4th fastest growing Metro',
+                'Highest rent growth in the Nation',
+                'Maricopa County is the fastest growing US County for the 3rd straight year',
+                '17% employment growth and 76% wage growth projected over the next decade',
+            ],
+        },
+    ],
 };
