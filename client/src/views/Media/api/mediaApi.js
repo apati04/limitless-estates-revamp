51bb149067448f189fca6189e82f34b3ac30940e
Andrew Patipaksiri <apatipaksiri@where2getit.com>
2020-02-06 20:30:35 -0800

    media

--- client/src/views/Media/api/mediaApi.js
+++ client/src/views/Media/api/mediaApi.js
@@ -3,6 +3,36 @@ import shortid from 'shortid';
 // media api
 export default [
     {
+        title:
+            'Kyle Mitchell: Scaling from a $1.5M deal to a $15M deal in 1 year.',
+        show: 'The Capital Raiser Show',
+        id: shortid.generate(),
+        author: 'Ruben Greth ft. Kyle Mitchell',
+        url:
+            'https://podcasts.apple.com/us/podcast/kyle-mitchell-scaling-from-a-$1-5m-deal-to-a-$15m-deal-in-1-year/id1478498436?i=1000462498883',
+        thumbnail:
+            'https://is2-ssl.mzstatic.com/image/thumb/Podcasts113/v4/d9/9c/d9/d99cd9a2-0172-71bf-215b-4fd5ae248a2a/mza_2129630102826244846.jpg/626x0w.jpg',
+        bgPos: '',
+        description:
+            'Kyle Mitchell goes over his background, transitioning into multifamily, building a team, investor relations and communication with the client database, modeling successful syndicators, tools for building your team and creating a investor pool, creating his first ppm, buying the deal before it gets to market, reverse engineering deals for success, podcasting and raising capital.',
+        duration: 43,
+    },
+    {
+        title:
+            'POWC #232 - Stay Patient & Stick to your Criteria with Kyle Mitchell',
+        show: 'Pillars Of Wealth Creation',
+        author: 'with Todd Dexheimer',
+        url:
+            'https://podcasts.apple.com/us/podcast/powc-232-stay-patient-stick-to-your-criteria-kyle-mitchell/id1296372835?i=1000461095832',
+        duration: 34,
+        thumbnail:
+            'https://i1.sndcdn.com/artworks-000656547703-i8ggod-t500x500.jpg',
+        id: shortid.generate(),
+        description: `Kyle hits on some very important tips for success. Stay patient and stick with your criteria, do things that others aren’t willing to, over communicate and treat it like a business.
+
+        Kyle Mitchell is a real estate entrepreneur who has a focus on Multifamily Syndication and currently has $17MM AUM. He is the Managing Partner and Co-Founder of Limitless Estates, who’s vision is to provide A class living to lower income housing by putting the residents first and instilling a sense of community while inspiring others to do the same. Kyle is also the co-host of the weekly real estate podcast, Passive Income through Multifamily Real Estate.`,
+    },
+    {
         title: 'Going Full-Out and All-In',
         show: 'Another Way to Play',
         id: shortid.generate(),
@@ -14,7 +44,7 @@ export default [
         bgPos: 'center top',
         description:
             'Kyle Mitchell is a real estate entrepreneur who just recently closed on his first multifamily syndication in May of 2019. He is the Managing Partner and Co-Founder of Limitless Estates, whose vision is to provide A-class living to lower income housing by putting the residents first and instilling a sense of community while inspiring others to do the same.</p>',
-        duration: 43
+        duration: 43,
     },
     {
         id: shortid.generate(),
@@ -27,7 +57,7 @@ export default [
         bgPos: 'top',
         description:
             ' In this episode, Dan Handford with PassiveInvesting.com interviews Kyle Mitchell about a 42-unit apartment complex called Midtown on 2nd located in Tucson, AZ that he acquired as an out-of-state owner.',
-        duration: 26
+        duration: 26,
     },
     {
         id: shortid.generate(),
@@ -40,7 +70,7 @@ export default [
         bgPos: 'top',
         description:
             'Kyle Mitchell is a real estate entrepreneur who just recently closed on his first multifamily syndication in May of 2019. He is the Managing Partner and Co-Founder of Limitless Estates, who’s vision is to provide A class living to lower income housing by putting the residents first and instilling a sense of community while inspiring others to do the same. Kyle is also the co-host of the weekly real estate podcast, Passive Income through Multifamily Real Estate.',
-        duration: 32
+        duration: 32,
     },
     {
         id: shortid.generate(),
@@ -52,7 +82,7 @@ export default [
             'https://targetmarketinsights.libsyn.com/ep-126-syndication-lessons-from-his-first-deal-with-kyle-mitchell',
         duration: 32,
         description:
-            'Doing your first syndication can be daunting, but Kyle Mitchell (along with his wife Lalita) dove deep into the learning process to complete their first syndication. He faced various challenges managing all of the steps but is already on to his next deal. He shares his key lessons from his first syndication, info on the Arizona market and more on his podcast, Passive Income through Multifamily Real Estate.'
+            'Doing your first syndication can be daunting, but Kyle Mitchell (along with his wife Lalita) dove deep into the learning process to complete their first syndication. He faced various challenges managing all of the steps but is already on to his next deal. He shares his key lessons from his first syndication, info on the Arizona market and more on his podcast, Passive Income through Multifamily Real Estate.',
     },
     {
         id: shortid.generate(),
@@ -64,8 +94,8 @@ export default [
             'https://realestateinvestingforwomen.libsyn.com/creating-the-roadmap-to-financial-independence-with-lalita-patipaksiri-kyle-mitchell',
         duration: 30,
         description: `Lalita Patipaksiri is currently Currently the General Manager at Los Amigos Golf Course in Downey, CA. Part-time she focuses on her and her fiancee's real estate company, Limitless-Estates, where they focus on multi family investing. They also host a Meet-up group in Long Beach, CA called Out-Of-State Apartment Investing, which has over 1,000 members and they just launched their podcast in March 2019.
-        
-        Kyle Mitchell is a more tenured individual in the real estate business.Starting with solo family rentals Kyle quickly realized that is multi- family units that create the best source of income.`
+
+        Kyle Mitchell is a more tenured individual in the real estate business.Starting with solo family rentals Kyle quickly realized that is multi- family units that create the best source of income.`,
     },
     {
         id: shortid.generate(),
@@ -75,10 +105,11 @@ export default [
         type: 'video',
         url: 'https://www.youtube.com/watch?v=kRI4gDBfulU',
         links: [
-            'https://podcasts.apple.com/us/podcast/episode-42-syndicating-apartment-building-in-arizona/id1382826261?i=1000441806299'
+            'https://podcasts.apple.com/us/podcast/episode-42-syndicating-apartment-building-in-arizona/id1382826261?i=1000441806299',
         ],
-        description: '<p>On this episode I sit down with Kyle Mitchell, a multifamily syndicator out of Los Angeles. I have been friends with Kyle for the last few years and he recently quit his job to do real estate full time and his goal was to syndicate some apartment buildings. He finally completed his first deals after years of work and study.</p>',
-        duration: 30
+        description:
+            '<p>On this episode I sit down with Kyle Mitchell, a multifamily syndicator out of Los Angeles. I have been friends with Kyle for the last few years and he recently quit his job to do real estate full time and his goal was to syndicate some apartment buildings. He finally completed his first deals after years of work and study.</p>',
+        duration: 30,
     },
     {
         id: shortid.generate(),
@@ -86,8 +117,9 @@ export default [
         author: 'with Michael Zuber',
         url: 'https://youtu.be/lp7iveQwOss',
         thumbnail: 'https://i.imgur.com/ZKxWJMV.jpg',
-        description: '<p>Real Estate Investing offers so many different paths and Options.  Hear how Kyle went from a CA Rental to 3 out of state markets and finally Apartment Buildings.</p>',
-        duration: 24
+        description:
+            '<p>Real Estate Investing offers so many different paths and Options.  Hear how Kyle went from a CA Rental to 3 out of state markets and finally Apartment Buildings.</p>',
+        duration: 24,
     },
     {
         id: shortid.generate(),
@@ -95,8 +127,9 @@ export default [
         author: 'Adam Adams',
         url: 'https://www.facebook.com/adam.aaa.adams/videos/952168237092/',
         thumbnail: 'https://i.imgur.com/C0gTpkY.jpg?1',
-        description: '<p>Kyle Mitchell has a super inspirational story you will want to hear. He will share how YOU can take action like he did. If you are wanting to take action, get success, and quit your job then this is a must listen to episode.</p><p>You will notice that Kyle took action on running a meetup AND hosting a podcast BEFORE he closed his first deal. Now after only about one year of getting started he is CRUSHING it in this business and he will share his system for you to do the same.',
-        duration: 27
+        description:
+            '<p>Kyle Mitchell has a super inspirational story you will want to hear. He will share how YOU can take action like he did. If you are wanting to take action, get success, and quit your job then this is a must listen to episode.</p><p>You will notice that Kyle took action on running a meetup AND hosting a podcast BEFORE he closed his first deal. Now after only about one year of getting started he is CRUSHING it in this business and he will share his system for you to do the same.',
+        duration: 27,
     },
     {
         id: shortid.generate(),
@@ -105,8 +138,9 @@ export default [
         url:
             'http://activedutypassiveincome.buzzsprout.com/183798/1341187-adpi_093-passive-income-through-multifamily-real-estate-with-kyle-mitchell',
         thumbnail: 'https://i.imgur.com/WMTkbZN.jpg',
-        description: 'In this awesome episode, we learn about the advantages of multifamily investing over single family. Kyle shares with us the lessons learned from single family investing and how he applied them to his multifamily investing career.',
-        duration: 28
+        description:
+            'In this awesome episode, we learn about the advantages of multifamily investing over single family. Kyle shares with us the lessons learned from single family investing and how he applied them to his multifamily investing career.',
+        duration: 28,
     },
     {
         id: shortid.generate(),
@@ -115,7 +149,8 @@ export default [
         url: 'https://youtu.be/BQ65azTVTrU',
         author: 'with Jacqueline Huynh',
         duration: 27,
-        description: 'Our guest, Kyle Mitchell, is a real estate investor, entrepreneur, and successful business owner.   He owns properties in 3 different markets (Illinois, Ohio & Arkansas) and just closed on a 42 multifamily unit syndication in Tucson, Arizona <br/> He is the co-host of a weekly real estate podcast called “Passive Income through Multifamily Real Estate” where he speaks with various experts in the real estate industry to help educate and create clarity for passive investors.'
+        description:
+            'Our guest, Kyle Mitchell, is a real estate investor, entrepreneur, and successful business owner.   He owns properties in 3 different markets (Illinois, Ohio & Arkansas) and just closed on a 42 multifamily unit syndication in Tucson, Arizona <br/> He is the co-host of a weekly real estate podcast called “Passive Income through Multifamily Real Estate” where he speaks with various experts in the real estate industry to help educate and create clarity for passive investors.',
     },
     {
         id: shortid.generate(),
@@ -126,7 +161,8 @@ export default [
             'https://lifebridgecapital.com/2019/07/ws269-the-impacts-of-your-first-deal-with-kyle-mitchell',
         thumbnail: 'https://i.imgur.com/iWfeVtr.jpg',
         duration: 24,
-        description: 'Real estate will always be tricky, and the only way to be successful in it is by having the right people teach you the right things. Kyle Mitchell, the Managing Partner and Co-founder of Limitless Estates, shares the story of how he got started with investing in single-family real estate back in 2013 and eventually focusing on syndication. Going through his first syndication deal, Kyle narrates the importance of coaching programs in preparing yourself for landing that first deal. Now, with the track record that he’s built, he shares some of the processes he’s implemented and some tips on how he built trust with investors.'
+        description:
+            'Real estate will always be tricky, and the only way to be successful in it is by having the right people teach you the right things. Kyle Mitchell, the Managing Partner and Co-founder of Limitless Estates, shares the story of how he got started with investing in single-family real estate back in 2013 and eventually focusing on syndication. Going through his first syndication deal, Kyle narrates the importance of coaching programs in preparing yourself for landing that first deal. Now, with the track record that he’s built, he shares some of the processes he’s implemented and some tips on how he built trust with investors.',
     },
     {
         id: shortid.generate(),
@@ -140,7 +176,7 @@ export default [
         description: `<p>ABOUT</p><br/>
 <p>Welcome to the Money Matters Top Tips Podcast with Adam Torres where business owners, entrepreneurs and executives reveal their top tips for success.</p>
 <p>Follow Adam on Instagram at Ask Adam Torres for up to date information on book releases and tour schedule.</p>
-<p>Apply to be interviewed by Adam on our podcast: <a href="www.moneymatterstoptips.com">www.moneymatterstoptips.com</a> / podcastguest</p>`
+<p>Apply to be interviewed by Adam on our podcast: <a href="www.moneymatterstoptips.com">www.moneymatterstoptips.com</a> / podcastguest</p>`,
     },
     {
         id: shortid.generate(),
@@ -151,7 +187,7 @@ export default [
         thumbnail: 'https://i.imgur.com/aRtJP9V.jpg',
         duration: 19,
         description:
-            'Kyle left his full time job just eight months ago (as of this recording) and already closed on his first multifamily syndication. He started out in real estate like many of us, working full time and buying single family homes. Hear how and why he was confident in leaving his job, and what it took to close on his first syndication. If you enjoyed today’s episode remember to subscribe in iTunes and leave us a review! Best Ever Tweet: “Real estate is a team sport” - Kyle Mitchell'
+            'Kyle left his full time job just eight months ago (as of this recording) and already closed on his first multifamily syndication. He started out in real estate like many of us, working full time and buying single family homes. Hear how and why he was confident in leaving his job, and what it took to close on his first syndication. If you enjoyed today’s episode remember to subscribe in iTunes and leave us a review! Best Ever Tweet: “Real estate is a team sport” - Kyle Mitchell',
     },
     {
         id: shortid.generate(),
@@ -161,7 +197,7 @@ export default [
         thumbnail: 'https://i.imgur.com/gEcTchd.jpg',
         duration: 35,
         description:
-            'Once you get a multifamily deal under contract, the clock starts ticking. You have limited time to raise capital, so it’s super-important that you have a database of potential investors to call on. Today, Kyle Mitchell joins me to share the blueprint he used to raise $1M in 60 days for his first syndication!'
+            'Once you get a multifamily deal under contract, the clock starts ticking. You have limited time to raise capital, so it’s super-important that you have a database of potential investors to call on. Today, Kyle Mitchell joins me to share the blueprint he used to raise $1M in 60 days for his first syndication!',
     },
     {
         id: shortid.generate(),
@@ -171,7 +207,7 @@ export default [
             'https://anchor.fm/dreamchasers/episodes/DREAM-CHASERS-KYLE-MITCHELL--Ep63-e4o3kt',
         thumbnail: 'https://i.imgur.com/EpLb1qo.jpg',
         description: `<p>In this episode Kyle discusses:</p><br/>
-        <p>1) How golf has played such an instrumental role in his life</p><br/><p>2) Why he left his high paying job to pursue a future in real estate and thought leadership </p><br/><p>3) How having a podcast has put his business in a highly valuable position</p>`
+        <p>1) How golf has played such an instrumental role in his life</p><br/><p>2) Why he left his high paying job to pursue a future in real estate and thought leadership </p><br/><p>3) How having a podcast has put his business in a highly valuable position</p>`,
     },
     {
         id: shortid.generate(),
@@ -181,7 +217,7 @@ export default [
         thumbnail: 'https://i.imgur.com/j1CNNhh.jpg',
         description:
             '<p>Here is some of what you will learn: <br /></p>The value of getting out of your comfort zone <br /> Developing your reach as an operator<br/> Vetting the market <br />Framework for finding attractive areas <br />506(b) and 506(c) funding differences <br />Importance of having active fundraising partners <br /> The value of self development',
-        duration: 30
+        duration: 30,
     },
     {
         id: shortid.generate(),
@@ -200,7 +236,7 @@ Topics Covered:
 Understanding turnkey properties
 
 Moving from single family to multifamily Investor communications through webinars Transitioning from corporate work to entrepreneur Nuggets of Wealth`,
-        duration: 28
+        duration: 28,
     },
     {
         id: shortid.generate(),
@@ -212,6 +248,6 @@ Moving from single family to multifamily Investor communications through webinar
         author: 'with Dave Morgia & Kyle Mitchell',
         description:
             '<p>Kyle Mitchell and his wife Lalita Patipaksiri found themselves taking 7 hour road trips in order to make an impact on their market in Phoenix and Tucson, AZ. Their commitment to this arduous process was rewarded when after 6 months of making great connections within their market, they closed on a 42 unit property in a great area of Tucson</p>',
-        duration: 29
-    }
+        duration: 29,
+    },
 ];
